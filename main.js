094d6a47795bd16f9e0ef00064e8a8811313f300
mateusperera <110178817+mateusperera@users.noreply.github.com>
2024-06-06 09:45:45 -0300

    06/06

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
+const conteudo = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i < botoes.length; 1++){
+
+for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
-        for(let j = 0; j < botoes.length ; j++){
-        botoes[j].classList.remove("ativo")
+        for (let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo")
+            conteudo[j].classList.remove("ativo")
         }
-        botoes[i].classList.and("ativo")
+        botoes[i].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
     }
 }
