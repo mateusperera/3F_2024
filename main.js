32a79b3b03dc295e69b465607e15c322ffc99732
mateusperera <110178817+mateusperera@users.noreply.github.com>
2024-06-20 09:41:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date (2024,6,5,12,35)
+const tempoObjetivo2 = new Date (2024,6,15,00,28)
+const tempoObjetivo3 = new Date (2024,6,30,20,35)
+const tempoObjetivo4 = new Date (2024,11,13,12,35)
 
+let agora = new Date();
+contador[0].textContent=tempoObjetivo1 -"agora"
+contador[1].textContent=tempoObjetivo2 -"agora"
+contador[2].textContent=tempoObjetivo3 -"agora"
+contador[3].textContent=tempoObjetivo4 -"agora"
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
