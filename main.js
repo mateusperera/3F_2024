df342a81420fac465f18c680aad3cd21e18c931a
mateusperera <110178817+mateusperera@users.noreply.github.com>
2024-06-21 08:54:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,10 +7,22 @@ const tempoObjetivo3 = new Date (2024,6,30,20,35)
 const tempoObjetivo4 = new Date (2024,11,13,12,35)
 
 let agora = new Date();
-contador[0].textContent=tempoObjetivo1 -"agora"
-contador[1].textContent=tempoObjetivo2 -"agora"
-contador[2].textContent=tempoObjetivo3 -"agora"
-contador[3].textContent=tempoObjetivo4 -"agora"
+contador[0].textContent=tempoObjetivo1 -agora
+contador[1].textContent=tempoObjetivo2 -agora
+contador[2].textContent=tempoObjetivo3 -agora
+contador[3].textContent=tempoObjetivo4 -agora
+
+let segundos
+let minutos
+let horas
+let dias
+
+segundos = (tempoObjetivo1-agora)/1000;
+minutos = segundos/60;
+horas = minutos/60;
+dias = horas/24;
+
+contador[0].textContent= `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
