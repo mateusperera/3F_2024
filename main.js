2144c3b254b686f4a56373079d2b5780fc10ce16
mateusperera <110178817+mateusperera@users.noreply.github.com>
2024-09-06 08:49:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -43,10 +43,10 @@ function calculaTempo(tempoObjetivo) {
 
 function atualizaCronometro(){
 
-    document.getElementById("dias0").textContent = calculaTempo(tempo[0])[0]
-    document.getElementById('horas0').textContent = calculaTempo(tempo[0])[1]
-    document.getElementById('minutos0').textContent = calculaTempo(tempo[0])[2]
-    document.getElementById('segundos0').textContent = calculaTempo(tempo[0])   [3]
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0]
+    document.getElementById('horas0').textContent = calculaTempo(tempos[0])[1]
+    document.getElementById('minutos0').textContent = calculaTempo(tempos[0])[2]
+    document.getElementById('segundos0').textContent = calculaTempo(tempos[0])[3]
     for (let i=0; i<contadores.length;i++){
    //     contadores[i].textContent = calculaTempo(tempos[i]);  
     }
